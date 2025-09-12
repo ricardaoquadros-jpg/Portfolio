4a48f1c70aa32939170f1849948246c5a179a0ad
Ricardo Quadros <ricardaoquadros@gmail.com>
2025-09-12 16:21:47 +0000

    Altere para que fique do mesmo jeito que o php (_for element div_)

--- src/components/landing/skills.tsx
+++ src/components/landing/skills.tsx
@@ -5,8 +5,8 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 
 const allSkills = {
-  "Linguagens de Programação": ["Python", "Java", "C", "PHP", "CSS", "HTML"],
-  "Outras Linguagens": ["Assembly"],
+  "Linguagens de Programação": ["Python", "Java", "C", "HTML"],
+  "Outras Linguagens": ["PHP", "CSS", "Assembly"],
   "Banco de Dados": ["MySQL"],
   "Ferramentas e Tecnologias": ["Power BI", "Android Studio", "Pacote Office"],
   "Hardware": ["Montagem e manutenção de computadores"],
