c138f4812fc7547275f8f30e22850ac323a30e4f
Ricardo Quadros <ricardaoquadros@gmail.com>
2025-09-12 19:02:40 +0000

    Coloque mais branco (_for element h1_)

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -9,7 +9,7 @@ export function Hero() {
       <div className="absolute inset-0 bg-grid-pattern bg-grid-pattern opacity-10"></div>
       <div className="container relative z-20 px-4 md:px-6">
         <div className="flex flex-col items-center space-y-4">
-          <h1 className="font-headline text-4xl font-bold tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl text-transparent bg-clip-text bg-gradient-to-r from-primary/70 to-primary">
+          <h1 className="font-headline text-4xl font-bold tracking-tighter sm:text-6xl md:text-7xl lg:text-8xl text-transparent bg-clip-text bg-gradient-to-r from-white to-primary">
             Ricardo Quadros
           </h1>
           <p className="max-w-[700px] text-foreground/80 md:text-xl">
