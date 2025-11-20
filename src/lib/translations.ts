32c3d3661ea3ef0dc348d8cfa67d26f258d14ee6
Ricardo Quadros <ricardaoquadros@gmail.com>
2025-11-20 21:31:35 +0000

    Desfazer

--- src/lib/translations.ts
+++ src/lib/translations.ts
@@ -120,7 +120,6 @@ export const translations: Record<'pt-BR' | 'en', Translation> = {
             { href: "#skills", label: "Competências" },
             { href: "#projects", label: "Projetos" },
             { href: "#contact", label: "Contato" },
-            { href: "/playground", label: "Playground" },
         ],
         header: {
             openMenu: "Abrir menu",
@@ -280,7 +279,6 @@ export const translations: Record<'pt-BR' | 'en', Translation> = {
             { href: "#skills", label: "Skills" },
             { href: "#projects", label: "Projects" },
             { href: "#contact", label: "Contact" },
-            { href: "/playground", label: "Playground" },
         ],
         header: {
             openMenu: "Open menu",
